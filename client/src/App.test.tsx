b154451a1809e192e4886bcb59fd7aebf31dea1b
pwput <45051480+pwput@users.noreply.github.com>
2023-10-25 19:38:53 +0200

    add workflows

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import {render, screen} from '@testing-library/react';
 import App from './App';
+import {BrowserRouter} from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Gene from "./pages/gene/gene";
+import Footer from "./components/Footer/Footer";
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  render(
+      <Footer/>,
+  );
+  // const linkElement = screen.getByText(/GenExVar/);
+  // expect(linkElement).toBeInTheDocument();
 });
